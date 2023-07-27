b35069a27f8116471e82f97e6e55f7f26b11435f
ripassorker2 <ripassorkar08@gmail.com>
2023-07-27 13:17:41 +0600

    setup project and get book route created

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gvjclco.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const booksCollection = client.db("practiceDB").collection("books");
+    const userCollection = client.db("practiceDB").collection("usersCol");
+
+    //<<------------ books route start---------------->>//
+    app.get("/books", async (req, res) => {
+      const result = await booksCollection.find({}).sort({ $natural: -1 }).toArray();
+      res.send(result);
+    });
+
+
+  } finally {
+  }
+}
+
+run().catch((err) => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running...on ${port}`);
+});
